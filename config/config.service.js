868fba324942bf01e45ab79d1d2ebad14387e43f
AbrarKhalil26 <abrarkhalil135@gmail.com>
2026-04-15 20:47:23 +0200

    remove config and node_modules from github

--- config/config.service.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import { resolve } from "node:path";
-
-const NODE_ENV = process.env.NODE_ENV || "development";
-dotenv.config({ path: resolve(`config/.env.${NODE_ENV}`) });
-
-export const PORT = +process.env.PORT || 3000;
-export const DB_URL = process.env.DB_URL;
-export const WHITE_LIST = process.env.WHITE_LIST?.split(",") || [];
